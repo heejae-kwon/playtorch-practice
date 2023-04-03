ad7cd551b7ee6bfe8549f9128e568517c2eaffaa
Heejae Kwon <sfhy33@gmail.com>
2023-04-03 16:13:20 +0900

    yoloV5 exmaple base done

--- App.tsx
+++ App.tsx
@@ -8,112 +8,52 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import { useCallback, useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import CameraScreen from './screens/CameraScreen';
+import LoadingScreen from './screens/LoadingScreen';
+import ResultsScreen from './screens/ResultsScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const ScreenStates = {
+  CAMERA: 0,
+  LOADING: 1,
+  RESULTS: 2,
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [image, setImage] = useState(null);
+  const [boundingBoxes, setBoundingBoxes] = useState(null);
+  const [screenState, setScreenState] = useState(ScreenStates.CAMERA);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // Handle the reset button and return to the camera capturing mode
+  const handleReset = useCallback(async () => {
+    setScreenState(ScreenStates.CAMERA);
+    if (image != null) {
+      await image.release();
+    }
+    setImage(null);
+    setBoundingBoxes(null);
+  }, [image, setScreenState]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      {screenState === ScreenStates.CAMERA && <CameraScreen />}
+      {screenState === ScreenStates.LOADING && <LoadingScreen />}
+      {screenState === ScreenStates.RESULTS && (
+        <ResultsScreen
+          image={image}
+          boundingBoxes={boundingBoxes}
+          onReset={handleReset}
+        />
+      )}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
   },
 });
 
